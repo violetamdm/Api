01e1297e99cb3a757ef4fc6062dabb357dae1b9d
violetamdm <violeta.macias@spain.vinci-energies.net>
2023-01-27 12:38:23 +0100

    test nuevo

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 import models
 from database import engine
 import schemas
-from endpoints import home, get_img
+from endpoints import home, get_img, create_burguer_bien
 from fastapi.testclient import TestClient
 from fastapi import APIRouter
 from unittest import TestCase
 from fastapi.testclient import TestClient
-
+from database import SessionLocal
+import crud
 #Por consola:
 # ir a proyectos\Api
 #Ejecutar comando: 
@@ -24,18 +25,30 @@ class TryTesting(TestCase):
 def test_get_home_connection():
     assert home() ==  { "mensaje" : "Usted se encuentra en la /home de la app bienvenido" }
 
+def test_post():
+    db  = SessionLocal()
+    burgueraux = schemas.Burguer(id=1,is_active=False,ingredientes="ole", nombre="sudo", imagen="b.jpg")
+    burgueraux2 = create_burguer_bien("sudo", "ole",0,"b.jpg", db)
+    assert  burgueraux2.nombre == burgueraux.nombre
+    assert  burgueraux2.ingredientes == burgueraux.ingredientes
+    assert  burgueraux2.imagen == burgueraux.imagen
+    assert  burgueraux2.is_active == burgueraux.is_active
 
-def test_get_home_connection():
-    assert home() ==  { "mensaje" : "Usted se encuentra en la /home de la app bienvenido" }
+'''def test_get_img_connection():
+    db  = SessionLocal()
+    b = crud.get_burguer_by_id(burguer_id=1, db=db)
+    i = crud.get_img(b)
+    assert get_img(1, db) == open(i, "r")'''
 
 def test_database_burguer():
     burgueraux = {
             "id": 1,
             "is_active": False,
             "ingredientes": "ole",
-            "nombre": "sudo"
+            "nombre": "sudo", 
+            "imagen": "b.jpg"
             }
-    newbur =  schemas.Burguer(id=1,is_active=False,ingredientes="ole", nombre="sudo")
+    newbur = schemas.Burguer(id=1,is_active=False,ingredientes="ole", nombre="sudo", imagen="b.jpg")
     assert newbur == burgueraux
 
 #ARREGLAR
